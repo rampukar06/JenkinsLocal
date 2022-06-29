959e84e40889d4ddf71c90299ad5bf82ddc5c24b
rampukar06 <107983698+rampukar06@users.noreply.github.com>
2022-06-29 08:17:43 +0530

    Update Program.cs
    Change program.csfile

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,8 +9,9 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.Write("Hello world!");
+        {            
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Welcome!");
         }
     }
 }
